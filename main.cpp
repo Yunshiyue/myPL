b8c6a26c6b32885b2a70483a0c4df80de7ae5c35
whh-buaa <892516165@qq.com>
2022-12-07 13:25:30 +0800

    为main函数增加-h -t参数

--- main.cpp
+++ main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include "Expression.h"
 #include "ProgramBlock.h"
+#include <string.h>
 
 extern qwq::ProgramBlock* programBlock;
 extern int yyparse();
 extern int yylex_destroy();
 extern FILE* yyin;
+void usage();
 
 int main(int argc, char** argv) {
-    std::string fileName;
+    std::string fileName = "";
+    bool printTree = false;
 
     if (argc == 1) {
         fileName = "/home/qingyue/data/cpp/myPL/examples/test_array_2d.qwq";
     } else {
-        fileName = argv[1];
-
+        for (int i = 1; i < argc; i++) {
+            if (!strcmp(argv[i] , "-t")) {
+                printTree = true;
+            } else if (!strcmp(argv[i] , "-h")) {
+                usage();
+                return 0;
+            } else {
+                if (fileName.length() == 0) {
+                    fileName = argv[i];
+                } else {
+                    // 出现了两个非参数的变量,错误
+                    std::cout << "参数错误" << "\n";
+                    usage();
+                    return 1;
+                }
+            }
+        }
     }
 
     yyin = fopen(fileName.c_str(), "r+");
@@ -30,13 +48,24 @@ int main(int argc, char** argv) {
         return 1;
     }
     std::cout << "grammaAnalyze success" << std::endl;
-    std::cout << "ast:" << std::endl;
-    programBlock->printAst(1);
-    std::cout << "ast-end" << std::endl;
+    // 如果输入了-t参数，需要输出语法树
+    if (printTree) {
+        std::cout << "ast:" << std::endl;
+        programBlock->printAst(1);
+        std::cout << "ast-end" << std::endl;
+    }
     std::cout << "start executing" << std::endl;
     programBlock->eval();
     std::cout << "finished" << std::endl;
     fclose(yyin);
     yylex_destroy();
     return 0;
+}
+
+void usage()
+{
+   std::cout << "Usage:\n";
+   std::cout << "./myPL filename -t -h\n";
+   std::cout << "\t-h this help text.\n";
+   std::cout << "\t-t tell qwq to print ast.\n";
 }
